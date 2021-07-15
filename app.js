3c61a903f286ffc5c45ce229c28cdb494b7614a6
astrid <nathanmcninch@gmail.com>
2021-07-15 12:15:00 -0700

    add questions to app.js file

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-// add this JavaScript file to index.html in the head element using the appropriate attribute
+// step 1 add this JavaScript file to index.html in the head element using the appropriate attribute
 
+//write functions for all of the following
+//use querySelector or querySelectorAll for all of the following, where needed.
+
+//step 2 select the first paragraph in the second div and add the class 'paragraph-color' so that the styles in the style sheet are applied to only that paragraph
+
+// step 3 create a paragraph template using a template litteral
+
+// step 5 add the paragraph template you just created to the first div using innerHTML
